18fb897b9a24bc4953712602f869f8639930a449
buba <buba@bubas-MacBook-Air.local>
2024-11-08 22:39:25 +0700

    Initial Files.

---
+++ example/titledb_image_downloader_example.dart
@@ -0,0 +1,6 @@
+import 'package:titledb_image_downloader/titledb_image_downloader.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/titledb_image_downloader_test.dart
@@ -0,0 +1,16 @@
+import 'package:titledb_image_downloader/titledb_image_downloader.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
